0cd7314b7bf4c5eddfbccb5e67d66eedde5d6384
hoangtularva <hoangtukg295@gmail.com>
2020-07-29 22:26:58 +0700

    Add Admin routes

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,5 +1,9 @@
 Samurai::Core::Engine.routes.draw do
   devise_for :users, class_name: "Samurai::User", module: :devise
 
+  namespace :admin do
+    get '/' => 'admin#index'
+  end
+  
   root to: "dashboard#index"
 end

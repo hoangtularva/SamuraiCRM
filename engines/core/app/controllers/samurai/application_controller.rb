88340b74c79de48cab8a0961966b52670140e66f
hoangtularva <hoangtukg295@gmail.com>
2020-07-29 23:10:34 +0700

    Fix the current_ability

--- engines/core/app/controllers/samurai/application_controller.rb
+++ engines/core/app/controllers/samurai/application_controller.rb
@@ -1,5 +1,9 @@
 module Samurai
   class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    def current_ability
+      @current_ability ||= Samurai::Ability.new(current_user)
+    end
   end
 end

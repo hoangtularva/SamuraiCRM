f5629bcfdcbbe5bcb43f3129b32f4ac823320dfd
hoangtularva <hoangtukg295@gmail.com>
2020-08-04 14:09:12 +0700

    devise user

--- engines/core/config/initializers/devise.rb
+++ engines/core/config/initializers/devise.rb
@@ -9,6 +9,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.router_name = :samurai
+  config.parent_controller = 'Samurai::ApplicationController'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.

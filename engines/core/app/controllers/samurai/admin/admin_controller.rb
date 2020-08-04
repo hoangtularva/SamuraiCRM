fe695dbeb2452fdd759c8d6c66ae7fc57cbaecb6
hoangtularva <hoangtukg295@gmail.com>
2020-08-04 15:25:52 +0700

    admin

---
+++ engines/core/app/controllers/samurai/admin/admin_controller.rb
@@ -0,0 +1,11 @@
+module Samurai
+    module Admin
+      class AdminController < ApplicationController
+        #authorize_resource class: false
+
+        def index
+         
+        end
+      end
+    end
+  end
---
+++ engines/core/app/controllers/samurai/admin/users_controller.rb
@@ -0,0 +1,11 @@
+module Samurai
+  module Admin
+    class UsersController < AdminController
+
+      def index
+        @users = Samurai::User.ordered
+      end
+
+    end
+  end
+end

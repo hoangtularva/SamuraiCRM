83de870b74562bf373811f988035aa50201a0dd3
hoangtularva <hoangtukg295@gmail.com>
2020-08-11 14:47:43 +0700

    Rename the table in the generated migration.

--- engines/tasks/db/migrate/20200811074509_create_samurai_tasks_tasks.rb
+++ engines/tasks/db/migrate/20200811074509_create_samurai_tasks_tasks.rb
@@ -3,8 +3,8 @@ class CreateSamuraiTasksTasks < ActiveRecord::Migration[5.1]
     create_table :samurai_tasks_tasks do |t|
       t.string :title
       t.text :content
-      t.references :user, foreign_key: true
-      t.references :contact, foreign_key: true
+      t.references :user, foreign_key: { to_table: :samurai_users}
+      t.references :contact, foreign_key: { to_table: :samurai_contacts_contacts}
 
       t.timestamps
     end

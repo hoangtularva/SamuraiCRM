fc08413758cbca7f09c76e21aab058226a3f959c
hoangtularva <hoangtukg295@gmail.com>
2020-08-11 17:33:28 +0700

    Extend the admin panel - finish chapter 6

--- engines/tasks/app/models/samurai/tasks/task.rb
+++ engines/tasks/app/models/samurai/tasks/task.rb
@@ -5,5 +5,7 @@ module Samurai::Tasks
     if Samurai::Core.available?(:contacts)
       belongs_to :contact, class_name: 'Samurai::Contacts::Contact'
     end
+
+    scope :ordered, -> { order('created_at desc') }
   end
 end
--- engines/tasks/config/routes.rb
+++ engines/tasks/config/routes.rb
@@ -1,5 +1,9 @@
 Samurai::Core::Engine.routes.draw do
     scope module: 'tasks' do
         resources :tasks
-    end
+    
+        namespace :admin do
+          resources :tasks, only: :index
+        end
+      end
 end
